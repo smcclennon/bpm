a2c9a1fa7eae56aa7bb8007a21b506736d267deb
Shiraz <smcclennon@protonmail.com>
2022-01-19 19:10:26 +0000

    Simplify code (accuracy of 1), add comments

--- bpm.py
+++ bpm.py
@@ -1,19 +1,15 @@
 import time
 
-def check_bpm(loops):
-    beats = 0  # Store number of beats recorded
-    start_time = time.time()  # Store start time
-
-    for loop in range(loops):
-        input()  # Allow user to press "Enter" to the beat
-        beats += 1  # Count new beats
-
-    end_time = time.time()  # Store end time after loop completes
-    duration = end_time - start_time  # Calculate duration taken for x beats
-    relative_bpm = 60 / duration  # Calculate relative minute
-    bpm = relative_bpm * beats  # Calculate BPM
+# Calculate BPM
+def check_bpm():
+    start_time = time.time()  # Time before user pressed Enter
+    input()  # Pause until user has pressed Enter
+    end_time = time.time()  # Time after user pressed enter
+    duration = end_time - start_time  # Time taken to press Enter
+    bpm = 60 / duration  # Calculate BPM
     return bpm
 
-print('Press enter to the beat')
-bpm = check_bpm(4)
-print(f'BPM: {bpm}')
+input('Press enter to the beat')
+for i in range(4):
+    bpm = check_bpm()
+    print(f'BPM: {bpm}')
