9bdd24b6ca7b704b148db6c40f8b4ca71259bdf7
Sourcery AI <>
2022-07-13 10:35:31 +0000

    'Refactored by Sourcery'

--- bpm.py
+++ bpm.py
@@ -5,9 +5,7 @@ debug = False
 # Calculate average value in a list
 def average(value_list):
     list_length = len(value_list)  # Number of items in list
-    list_sum = 0  # Track sum of all values
-    for value in value_list:  # For value in list
-        list_sum += value  # Add actual value
+    list_sum = sum(value_list)
     return list_sum / list_length  # Calculate value average
 
 # Calculate BPM
@@ -16,8 +14,7 @@ def check_bpm():
     input('\033[F')  # Pause until user has pressed Enter, move cursor up once
     end_time = time.time()  # Time after user pressed enter
     duration = end_time - start_time  # Time taken to press Enter
-    bpm = 60 / duration  # Calculate BPM
-    return bpm
+    return 60 / duration
 
 def continuous_bpm(loops):
     bpms = []  # Store recorded bpms to calculate average
@@ -25,14 +22,13 @@ def continuous_bpm(loops):
         bpms.append(check_bpm())  # Add current bpm to list
         bpms_length = len(bpms)  # Number of bpms in list
         average_bpm = average(bpms)  # Get average bpm in list
-        
+
         print(f'Average BPM: {average_bpm}   ', end='')  # Print average bpm with no line break
         if debug: print(f'{loop}')
-        
-        if bpms_length > 4:  # If list length above 4
-            if (loop) % 2 == 0:  # If loop is on even increment
-                if debug: print(f'\tPURGING: {bpms[0]}')
-                del bpms[0]  # Delete the oldest value in the list
+
+        if bpms_length > 4 and (loop) % 2 == 0:
+            if debug: print(f'\tPURGING: {bpms[0]}')
+            del bpms[0]  # Delete the oldest value in the list
 
 input('Press enter to the beat')
 continuous_bpm(32)
